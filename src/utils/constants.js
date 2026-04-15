523bbb3982c39a131ba74169d365a7418df1af4c
Nitin Kumar <155599140+nitin16112004@users.noreply.github.com>
2026-04-15 13:11:14 +0530

    Update API_BASE_URL to production endpoint

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = "https://taskflow-backend-6a3u.onrender.com/api";
 
 export const ROUTES = {
   LOGIN: "/login",
